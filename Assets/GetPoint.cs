8a3504a9d5a340d05f6b83dbb76c83a3639fe2ae
MioOsaka <mosaka@ths.co.jp>
2018-09-17 17:42:33 +0900

    得点表示の課題が完成しました。発展課題は未完了です。

---
+++ Assets/GetPoint.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class GetPoint : MonoBehaviour {
+	public int addPoint;
+
+	//ポイントを表示するテキスト
+	private GameObject showPoint;
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のGameOverTextオブジェクトを取得
+		this.showPoint = GameObject.Find("PointText");
+	}
+	// Update is called once per frame
+	void Update () {		
+	}
+
+	//衝突時に呼ばれる関数
+	void OnCollisionEnter(Collision other) {
+		// タグによって追加の点数を変える
+		if (tag == "SmallStarTag") {
+			addPoint = 10;
+		} else if (tag == "LargeStarTag") {
+			addPoint = 20;
+		} else if (tag == "SmallCloudTag") {
+			addPoint = 30;
+		} else if (tag == "LargeCloudTag") {
+			addPoint = 50;
+		}
+
+		this.showPoint.GetComponent<ShowPoint>().point += addPoint;
+
+	}
+
+}
+	
---
+++ Assets/ShowPoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPoint : MonoBehaviour {
+
+	//ポイントを表示するテキスト
+	private GameObject showPoint;
+	//ポイント初期値
+	public int point = 0;
+	//ポイント文字列
+	private string pointStr;
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のGameOverTextオブジェクトを取得
+		this.showPoint = GameObject.Find("PointText");
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		//得点を取得した場合
+		pointStr = point.ToString ();
+		//PointTextに合計点を表示
+		this.showPoint.GetComponent<Text>().text= pointStr;
+
+//		Debug.Log(pointStr);
+
+	}
+}
